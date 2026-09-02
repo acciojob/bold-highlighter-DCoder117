dd5c6f047c6df956c6cccbb0dceb2f6ff8da745e
DCoder117 <dhirajopjadhav17@gmail.com>
2026-09-02 11:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function highlight() {
     //Write your code here
+function highlight() {
 
+    let strongWords = document.querySelectorAll("strong");
+
+    strongWords.forEach(function(word) {
+        word.style.color = "green";
+    });
+
+}
+
+
+function return_normal() {
+
+    let strongWords = document.querySelectorAll("strong");
+
+    strongWords.forEach(function(word) {
+        word.style.color = "black";
+    });
+
+}
 
 }
 
